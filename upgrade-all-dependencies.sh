671b7c254a2bdff432782a355f534f9e6910a1a2
kiki-kanri <a470666@gmail.com>
2024-08-23 17:08:13 +0800

    chore: add basic files

---
+++ upgrade-all-dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cd $(dirname "$(readlink -f "$0")")
+SCRIPT_NAME="upgrade-dependencies.sh"
+
+execute_script() {
+	local dir=$1
+	cd "$dir"
+	echo "Executing $SCRIPT_NAME in $dir ..."
+	chmod 700 $SCRIPT_NAME
+	./$SCRIPT_NAME
+}
+
+find . -type f -name "$SCRIPT_NAME" | while read -r file; do
+	dir=$(dirname "$file")
+	execute_script "$dir"
+done
