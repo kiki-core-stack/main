66239fbefff9fa22d59fc1a0b874359c3b221784
kiki-kanri <a470666@gmail.com>
2024-08-23 23:39:06 +0800

    chore: update scripts

--- tmux-run-all-dev.sh
+++ tmux-run-all-dev.sh
@@ -5,10 +5,12 @@ SCRIPT_NAME="tmux-run-dev.sh"
 
 execute_script() {
 	local dir=$1
-	cd "$dir"
-	echo "Executing $SCRIPT_NAME in $dir ..."
-	chmod 700 $SCRIPT_NAME
-	./$SCRIPT_NAME
+	(
+		cd "$dir"
+		echo "Executing $SCRIPT_NAME in $dir ..."
+		chmod 700 $SCRIPT_NAME
+		./$SCRIPT_NAME
+	)
 }
 
 find . -type f -name "$SCRIPT_NAME" | while read -r file; do
