6fec79b81b85996a7c4279a4d04bd5c10e278902
kiki-kanri <a470666@gmail.com>
2024-09-14 23:55:51 +0800

    chore: update script

--- cleanup-and-upgrade-all-dependencies.sh
+++ cleanup-and-upgrade-all-dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd $(dirname "$(readlink -f "$0")")
-find . -name .nitro -type d -prune -exec rm -rf '{}' \;
-find . -name .nuxt -type d -prune -exec rm -rf '{}' \;
-find . -name .output -type d -prune -exec rm -rf '{}' \;
-find . -name bun.lockb -type f -prune -exec rm '{}' \;
-find . -name dist -type d -prune -exec rm -rf '{}' \;
-find . -name pnpm-lock.yaml -type f -prune -exec rm '{}' \;
-find . -name node_modules -type d -prune -exec rm -rf '{}' \;
+find . -name .nitro -type d -prune -exec rm -rf "{}" +
+find . -name .nuxt -type d -prune -exec rm -rf "{}" +
+find . -name .output -type d -prune -exec rm -rf "{}" +
+find . -name bun.lockb -type f -prune -exec rm "{}" +
+find . -name dist -type d -prune -exec rm -rf "{}" +
+find . -name pnpm-lock.yaml -type f -prune -exec rm "{}" +
+find . -name node_modules -type d -prune -exec rm -rf "{}" +
 ./upgrade-all-dependencies.sh
