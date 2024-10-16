24b8a3a560797210d12b78eee06198614846eb4a
kiki-kanri <a470666@gmail.com>
2024-10-16 14:51:21 +0800

    chore: update example env, docker-compose, and script

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -2,7 +2,6 @@
 
 if [ "$1" = '-p' ]; then
 	docker compose pull &&
-		docker pull alpine:latest &&
 		docker pull busybox:latest &&
 		docker pull kikikanri/node22:base-alpine &&
 		docker pull node:22-alpine
