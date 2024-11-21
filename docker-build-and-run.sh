fe63f7c940973f21a1522fd6d702664dba732bd8
kiki-kanri <a470666@gmail.com>
2024-11-21 18:25:06 +0800

    style: change scripts indentation to 4 spaces

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 if [ "$1" = '-p' ]; then
-	docker compose pull &&
-		docker pull busybox:latest &&
-		docker pull kikikanri/node22:base-alpine &&
-		docker pull node:22-alpine
+    docker compose pull &&
+        docker pull busybox:latest &&
+        docker pull kikikanri/node22:base-alpine &&
+        docker pull node:22-alpine
 fi
 
 docker compose build && docker compose up -d --remove-orphans
--- upgrade-all-dependencies.sh
+++ upgrade-all-dependencies.sh
@@ -4,16 +4,16 @@ cd "$(dirname "$(readlink -f "$0")")"
 SCRIPT_NAME='upgrade-dependencies.sh'
 
 execute_script() {
-	local dir=$1
-	(
-		cd "$dir"
-		echo "Executing $SCRIPT_NAME in $dir ..."
-		chmod 700 "$SCRIPT_NAME"
-		./"$SCRIPT_NAME"
-	)
+    local dir=$1
+    (
+        cd "$dir"
+        echo "Executing $SCRIPT_NAME in $dir ..."
+        chmod 700 "$SCRIPT_NAME"
+        ./"$SCRIPT_NAME"
+    )
 }
 
 find . -type f -name "$SCRIPT_NAME" | while read -r file; do
-	dir=$(dirname "$file")
-	execute_script "$dir"
+    dir=$(dirname "$file")
+    execute_script "$dir"
 done
