b9911342026976d4a998a97d3fa9113c7fc94bdc
kiki-kanri <a470666@gmail.com>
2024-11-23 16:15:33 +0800

    chore: remove `&&` from pull images in `docker-build-and-run.sh`

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$1" = '-p' ]; then
-    docker compose pull &&
-        docker pull busybox:latest &&
-        docker pull kikikanri/node22:base-alpine &&
-        docker pull node:22-alpine &&
-        docker pull oven/bun:alpine
+    docker compose pull
+    docker pull busybox:latest
+    docker pull kikikanri/node22:base-alpine
+    docker pull node:22-alpine
+    docker pull oven/bun:alpine
 fi
 
 docker compose build && docker compose up -d --remove-orphans
