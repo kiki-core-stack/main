407cc74ffb40b6876cd10ca091fb2cca87e89905
kiki-kanri <a470666@gmail.com>
2024-11-23 16:14:47 +0800

    chore: add `oven/bun:alpine` to pulled images in `docker-build-and-run.sh`

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -4,7 +4,8 @@ if [ "$1" = '-p' ]; then
     docker compose pull &&
         docker pull busybox:latest &&
         docker pull kikikanri/node22:base-alpine &&
-        docker pull node:22-alpine
+        docker pull node:22-alpine &&
+        docker pull oven/bun:alpine
 fi
 
 docker compose build && docker compose up -d --remove-orphans
