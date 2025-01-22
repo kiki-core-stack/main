8f87611fc90ae961f8dcffc02bf055ff6ae5afc3
kiki-kanri <a470666@gmail.com>
2025-01-23 02:05:36 +0800

    chore: merge `find` commands in `cleanup-and-upgrade-all-dependencies.sh`

--- cleanup-and-upgrade-all-dependencies.sh
+++ cleanup-and-upgrade-all-dependencies.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
 cd "$(dirname "$(readlink -f "$0")")"
-find . -name '.cache' -type d -prune -exec rm -rf {} +
-find . -name '.nitro' -type d -prune -exec rm -rf {} +
-find . -name '.nuxt' -type d -prune -exec rm -rf {} +
-find . -name '.output' -type d -prune -exec rm -rf {} +
-find . -name 'bun.lockb' -type f -delete
-find . -name 'dist' -type d -prune -exec rm -rf {} +
-find . -name 'node_modules' -type d -prune -exec rm -rf {} +
-find . -name 'pnpm-lock.yaml' -type f -delete
+find . \
+    \( -name '.cache' \
+    -o -name '.nitro' \
+    -o -name '.nuxt' \
+    -o -name '.output' \
+    -o -name 'dist' \
+    -o -name 'node_modules' \
+    \) \
+    -type d \
+    -prune \
+    -exec rm -rf {} +
+
+find . \
+    \( -name 'bun.lock' \
+    -o -name 'bun.lockb' \
+    -o -name 'pnpm-lock.yaml' \
+    \) \
+    -type f \
+    -delete
+
 ./upgrade-all-dependencies.sh
 ./modify-files-permissions.sh
