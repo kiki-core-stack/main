b75215b6a0dcf88754fe6bfb2a2fd8ab25b82f9a
kiki-kanri <a470666@gmail.com>
2025-01-23 11:14:51 +0800

    chore: modify pulled images in `docker-build-and-run.sh`

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -6,8 +6,7 @@ if [[ "$*" == *'-p'* ]]; then
     docker compose pull &
     docker pull busybox:latest &
     docker pull kikikanri/node22:base-alpine &
-    docker pull node:22-alpine &
-    docker pull oven/bun:alpine &
+    docker pull oven/bun:slim &
     wait
 fi
 
