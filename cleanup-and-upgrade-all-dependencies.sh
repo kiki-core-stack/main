08e4442b78b0a67c547b0d7755e0e86c20f2a921
kiki-kanri <a470666@gmail.com>
2025-07-14 18:15:58 +0800

    chore: tidy up docker compose and scripts

--- cleanup-and-upgrade-all-dependencies.sh
+++ cleanup-and-upgrade-all-dependencies.sh
@@ -6,21 +6,21 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 cd "${SCRIPT_DIR}"
 
 find . \
-    \( -name '.cache' \
-    -o -name '.nitro' \
-    -o -name '.nuxt' \
-    -o -name '.output' \
-    -o -name 'dist' \
-    -o -name 'node_modules' \
+    \( -name .cache \
+    -o -name .nitro \
+    -o -name .nuxt \
+    -o -name .output \
+    -o -name dist \
+    -o -name node_modules \
     \) \
     -type d \
     -prune \
     -exec rm -rf {} +
 
 find . \
-    \( -name 'bun.lock' \
-    -o -name 'bun.lockb' \
-    -o -name 'pnpm-lock.yaml' \
+    \( -name bun.lock \
+    -o -name bun.lockb \
+    -o -name pnpm-lock.yaml \
     \) \
     -type f \
     -delete
