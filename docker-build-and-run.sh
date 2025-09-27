9f883e2a2628fd45d656efcded30d35647d883e1
kiki-kanri <a470666@gmail.com>
2025-09-28 03:21:11 +0800

    chore: update `docker-build-and-run.sh`

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -9,10 +9,8 @@ if [[ " $@ " =~ ' -p ' ]]; then
     docker compose pull &
     docker pull busybox:latest &
     docker pull docker/dockerfile:labs &
-    docker pull kikikanri/node:24-alpine &
     docker pull oven/bun:slim &
     wait
 fi
 
-COMPOSE_BAKE=true docker compose build
-docker compose up -d --remove-orphans
+docker compose up -d --build --remove-orphans
