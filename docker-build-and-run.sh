98711b48e7689edc87099dc32b7e46ece914af3a
kiki-kanri <a470666@gmail.com>
2025-11-05 13:36:39 +0800

    chore: update files

--- docker-build-and-run.sh
+++ docker-build-and-run.sh
@@ -9,6 +9,7 @@ if [[ " $@ " =~ ' -p ' ]]; then
     docker compose pull &
     docker pull busybox:latest &
     docker pull docker/dockerfile:labs &
+    docker pull node:24-alpine &
     docker pull oven/bun:slim &
     wait
 fi
